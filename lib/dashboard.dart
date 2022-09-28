d33b0f7c71ffa9cdcf66c17c968e4072ef1795b8
selviina <rinestiselvina@gmail.com>
2022-09-28 16:56:13 +0800

    data nasabah

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -14,12 +14,101 @@ class _dashboardState extends State<dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('Koperasi Undiksha'),
-        centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 10, 7, 139),
-      ),
-    );
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('Koperasi Undiksha'),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 10, 7, 139),
+        ),
+        body: SingleChildScrollView(
+            child: SafeArea(
+                child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Column(children: <Widget>[
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(20.0),
+                        // height: 100.0,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.0),
+                          border: Border.all(
+                            color: Color.fromARGB(255, 10, 7, 139),
+                          ),
+                        ),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                child: Image(
+                                  image: AssetImage(
+                                      'assets/images/logo-undiksha.png'),
+                                  width: 100,
+                                  height: 100,
+                                ),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(),
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        width: 300,
+                                        padding: EdgeInsets.all(20.0),
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(10.0),
+                                            color: Color.fromARGB(
+                                                255, 206, 191, 238)),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text('Nasabah',
+                                                style: TextStyle(
+                                                    fontWeight:
+                                                        FontWeight.bold)),
+                                            Text('Selvina Rinesti'),
+                                          ],
+                                        ),
+                                      ),
+                                      SizedBox(height: 10),
+                                      Container(
+                                        decoration: BoxDecoration(),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.start,
+                                          children: [
+                                            Container(
+                                              width: 300,
+                                              padding: EdgeInsets.all(20.0),
+                                              decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          10.0),
+                                                  color: Color.fromARGB(
+                                                      255, 206, 191, 238)),
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Text('Total Saldo Anda',
+                                                      style: TextStyle(
+                                                          fontWeight:
+                                                              FontWeight.bold)),
+                                                  Text('Rp. 1.000.000'),
+                                                ],
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      )
+                                    ]),
+                              ),
+                            ]),
+                      )
+                    ])))));
   }
 }
